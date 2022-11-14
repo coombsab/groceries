1735249fdbe7fbbde930a80129b7259846db3ed9
Abraham Coombs <coombsab@gmail.com>
2022-11-14 15:06:47 -0700

    styling on creator info card

--- groceries.client/src/components/CreatorInfo.vue
+++ groceries.client/src/components/CreatorInfo.vue
@@ -1,7 +1,10 @@
 <template>
-  <section class="creator-info">
-    <img :src="item?.creator.picture" :alt="item?.creator.name">
-    <span>{{item?.creator.name}}</span>
+  <section class="creator-info text-visible">
+    <h5 class="text-center">Created by</h5>
+    <div class="d-flex flex-wrap gap-3 align-items-center">
+      <img :src="item?.creator.picture" :alt="item?.creator.name">
+      <h2>{{item?.creator.name}}</h2>
+    </div>
   </section>
 </template>
 
@@ -21,5 +24,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.creator-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 
+img {
+  border-radius: 50%;
+  height: 64px;
+  width: 64px;
+}
 </style>
